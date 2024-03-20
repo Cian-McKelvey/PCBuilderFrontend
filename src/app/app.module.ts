da9a501d66225455f52f1c88ff23aa1459ab166f
Cian-McKelvey <cian.p.mckelvey@gmail.com>
2024-03-20 20:16:49 +0000

    Added tailwind css support, as well as routing + created a home component for the homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from "@angular/router";
+
+const routes: any = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule, // Allows Http requests
+    RouterModule.forRoot(routes), // Allows routing
   ],
   providers: [],
   bootstrap: [AppComponent]
